adc30e83e4683be6e7589ab679d71021d57350c4
Soufyan <MetaByte149@gmail.com>
2022-12-26 18:46:10 +0200

    Updates

---
+++ precache-manifest.ed38340a4bf23631906b23ebc8924c0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c3699dc20bff594ad0eb",
+    "url": "/souf-portfolio/static/css/main.5ed09568.chunk.css"
+  },
+  {
+    "revision": "c3699dc20bff594ad0eb",
+    "url": "/souf-portfolio/static/js/main.c3699dc2.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/souf-portfolio/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/souf-portfolio/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "b80b7e28cd903626b4ef",
+    "url": "/souf-portfolio/static/js/runtime~main.b80b7e28.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/souf-portfolio/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "790a01f9dfe4bc31c3bd10b12014e98e",
+    "url": "/souf-portfolio/index.html"
+  }
+];
